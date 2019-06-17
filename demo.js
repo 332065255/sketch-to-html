4a75a29d4ada40459511d5d823f61d68c62afef0
高凌霄 <gaolingxiao@meituan.com>
2019-06-17 11:29:52 +0800

    .

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
 var go = require('./index');
 var utils = require('./util');
-utils.isH5=true;
+utils.isH5=false;
 utils.isReact = true;
 utils.minWidthforPC = 1200;
-go('./旺铺宝app.sketch',()=>{
+go('./开店宝-调整（0614）.sketch',()=>{
     console.log('success');
 })
 //2html_Animation
